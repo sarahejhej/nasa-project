fc9d60888ec1dc3b44682b6d4f6f4e40b56a4836
sara <sara.heijkenskjold@cloudist.se>
2023-08-31 15:43:56 +0200

    Connect to Mongo cluster

--- server/src/server.js
+++ server/src/server.js
@@ -1,14 +1,26 @@
+require('dotenv').config();
 const http = require('http');
+const mongoose = require('mongoose');
 
 const app = require('./app');
 
 const { loadPlanetsData } = require('./models/planets.model');
 
 const PORT = process.env.PORT || 8000;
+const URL = process.env.MONGO_URL;
 
 const server = http.createServer(app);
 
+mongoose.connection.once('open', () => {
+  console.log('MongoDB connection ready!');
+});
+
+mongoose.connection.on('error', (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
